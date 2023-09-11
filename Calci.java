8dcf9502f76576537a35eec0f304869c6b0af21c
TharunMech <mtharun911@gmail.com>
2023-09-11 13:43:21 +0530

    added Driver Class

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+
 interface Calci{
 int add(int a,int b);
 }
@@ -5,5 +6,13 @@ int add(int a,int b);
 class Calciimpl implements Calci{
 public int add(int a,int b){
 return a+b;
+}
+}
+
+Class CalciDriver {
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int n=s.next;
+
 }
 }
